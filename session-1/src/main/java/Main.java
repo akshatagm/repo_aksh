0e23c6df9cb93e119757b0ce620b234843badea1
akshata <akshatamanturi97@gmail.com>
2024-08-11 23:11:44 +0530

    first com

---
+++ session-1/src/main/java/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        User ur=new User();
+        ur.age=10;
+        ur.name="abc";
+        System.out.println(ur.age+" "+ur.name);
+    }
+}
---
+++ session-1/src/main/java/User.java
@@ -0,0 +1,9 @@
+class User {
+    int age;
+    String name;
+
+    public void walk()
+    {
+        System.out.println("walking");
+    }
+}
